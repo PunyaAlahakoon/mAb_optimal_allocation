2dd4c0f05af03fa82082280d3f238595fc527fb5
Punya Alahakoon <punya.alahakoon@unsw.edu.au>
2024-01-23 10:20:32 +1100

    Create packages_and_load_func.R

---
+++ packages_and_load_func.R
@@ -0,0 +1,12 @@
+library(reshape2)
+library(ggplot2)
+library(data.table)
+library(ggpubr)
+library(RColorBrewer)
+library(ggthemes)
+library(dplyr)
+library(tidyr)
+# Run the R scripts in the R/ folder with your custom functions:
+# load all functions
+. <- lapply(list.files("R", full.names = TRUE), source)
+
